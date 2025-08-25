1740065d3e0dbd21445928e4671fbacc2fc81301
kostyayurchenko84 <kostyayurchenko84@yandex.ru>
2025-08-25 14:18:57 +0300

    relase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+  return matrix.flatMap((str, index) =>
+    index % 2 === 0 ? str : str.reverse()
+  );
 };
